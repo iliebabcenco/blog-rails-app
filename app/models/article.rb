41b0df686f7bf9c7746df9f647f8fc0928709103
felix_vondee <felixvondee8@gmail.com>
2021-04-16 09:55:44 +0200

    Changes for number nine

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
-    has_many :comments
-    validates :title, presence: true
-    validates :body, presence: true, length: { minimum: 10 }
+  include Visible
+
+  has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
